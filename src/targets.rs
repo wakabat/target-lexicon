c1bdf0edd133908762e240f21ff98615891c1017
wakabat <wakabat@protonmail.com>
2025-07-15 12:10:27 +0000

    Add riscv64im to available architecture list

--- src/targets.rs
+++ src/targets.rs
@@ -446,6 +446,7 @@ pub enum Riscv64Architecture {
     Riscv64,
     Riscv64gc,
     Riscv64imac,
+    Riscv64im,
 }
 
 impl Riscv64Architecture {
@@ -457,6 +458,7 @@ impl Riscv64Architecture {
             Riscv64 => Cow::Borrowed("riscv64"),
             Riscv64gc => Cow::Borrowed("riscv64gc"),
             Riscv64imac => Cow::Borrowed("riscv64imac"),
+            Riscv64im => Cow::Borrowed("riscv64im"),
         }
     }
 }
@@ -1187,6 +1189,7 @@ impl FromStr for Riscv64Architecture {
             "riscv64" => Riscv64,
             "riscv64gc" => Riscv64gc,
             "riscv64imac" => Riscv64imac,
+            "riscv64im" => Riscv64im,
             _ => return Err(()),
         })
     }
